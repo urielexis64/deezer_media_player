66de3b452c22b58ac819c3500d0ff540f3f1db53
Uriel Aispuro <urielalexis64@gmail.com>
2021-08-17 16:45:35 -0600

    add resume, pause and player status

--- lib/libs/music_player.dart
+++ lib/libs/music_player.dart
@@ -1,5 +1,8 @@
 import 'dart:async';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_sound_lite/flutter_sound.dart';
+
+enum MusicPlayerStatus { loading, error, playing, paused }
 
 class MusicPlayer {
   MusicPlayer() {
@@ -12,6 +15,18 @@ class MusicPlayer {
   }
 
   Completer<FlutterSoundPlayer> _completer = Completer();
+  ValueNotifier<MusicPlayerStatus> _status =
+      ValueNotifier(MusicPlayerStatus.loading);
+
+  ValueNotifier<MusicPlayerStatus> get status => _status;
+
+  bool get loading => _status.value == MusicPlayerStatus.loading;
+
+  bool get playing => _status.value == MusicPlayerStatus.playing;
+
+  bool get paused => _status.value == MusicPlayerStatus.paused;
+
+  bool get error => _status.value == MusicPlayerStatus.error;
 
   Future<void> _init() async {
     final player = await FlutterSoundPlayer().openAudioSession();
@@ -20,7 +35,19 @@ class MusicPlayer {
 
   Future<void> play(String uri) async {
     final player = await _completer.future;
-
     await player.startPlayer(fromURI: uri);
+    _status.value = MusicPlayerStatus.playing;
+  }
+
+  Future<void> resume() async {
+    final player = await _completer.future;
+    await player.resumePlayer();
+    _status.value = MusicPlayerStatus.playing;
+  }
+
+  Future<void> pause() async {
+    final player = await _completer.future;
+    await player.pausePlayer();
+    _status.value = MusicPlayerStatus.paused;
   }
 }
